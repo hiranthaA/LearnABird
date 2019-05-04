0ea1db1bf6530fd0b20aec44f81d1e0f1335f815
hiran <hiranthaathapaththu@gmail.com>
2019-05-04 20:26:25 +0530

    Bird details added. Bird details scrollable Play button added.

--- app/src/main/java/com/example/learnabird/DetailActivity.java
+++ app/src/main/java/com/example/learnabird/DetailActivity.java
@@ -2,6 +2,8 @@ package com.example.learnabird;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -9,19 +11,25 @@ public class DetailActivity extends AppCompatActivity {
 
     private ImageView iv_imgPreview;
     private TextView tv_birdName;
+    private TextView tv_birdDetails;
+    private Button btn_playSound;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
 
+        btn_playSound = findViewById(R.id.btn_play);
         iv_imgPreview = findViewById(R.id.imgPreview);
         tv_birdName = findViewById(R.id.txt_BirdName);
+        tv_birdDetails = findViewById(R.id.txt_birdDetails);
+        tv_birdDetails.setMovementMethod(new ScrollingMovementMethod());
 
         Bundle bundle = getIntent().getExtras();
         if(bundle!=null){
             tv_birdName.setText(bundle.getString("birdName"));
             iv_imgPreview.setImageResource(bundle.getInt("birdPic"));
+            tv_birdDetails.setText(bundle.getString("birdDetails"));
             getSupportActionBar().setTitle("Learn A Bird : "+bundle.getString("birdName"));
         }
     }
--- app/src/main/java/com/example/learnabird/MainActivity.java
+++ app/src/main/java/com/example/learnabird/MainActivity.java
@@ -12,6 +12,7 @@ public class MainActivity extends AppCompatActivity {
     private ListView lstBirds;
     private String[] arrBirdNames;
     private int[] arrBirdPics;
+    private String[] arrBirdDetails;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,15 +25,35 @@ public class MainActivity extends AppCompatActivity {
 
         lstBirds = findViewById(R.id.lstBirds);
 
-        arrBirdNames = new String[]{"Parrot", "Crow", "Pigeon", "Eagle", "Crane"};
+        arrBirdNames = new String[]{
+                "Parrot",
+                "Crow",
+                "Pigeon",
+                "Eagle",
+                "Crane"};
 
         arrBirdPics = new int[]{
-                R.mipmap.img_parrot_round,
-                R.mipmap.img_crow_round,
-                R.mipmap.img_pigeon_round,
-                R.mipmap.img_eagle_round,
-                R.mipmap.img_crane_round,
+                R.mipmap.img_parrot,
+                R.mipmap.img_crow,
+                R.mipmap.img_pigeon,
+                R.mipmap.img_eagle,
+                R.mipmap.img_crane,
         };
+
+        arrBirdDetails = new String[]{
+                "Parrots are members of the order Psittaciformes, which includes more than 350 bird species, including parakeets, macaws, cockatiels and cockatoos, according to the Integrated Taxonomic Information System (ITIS). Though there are many types of parrots, all parrot species have a few traits in common. For example, to be classified as a parrot, the bird must have a curved beak, and its feet must be zygodactyl, which means there are four toes on each foot with two toes that point forward and two that point backward.",
+                "Crows are black birds known for their intelligence and adaptability, and for their loud, harsh \"caw.\" They also have a reputation for damaging crops; however, their impact may be less than previously thought. \n" +
+                        "\n" +
+                        "The genus Corvus comprises crows, ravens and rooks. These birds are all part of the Corvidae family, which includes jays, magpies and nutcrackers.",
+                "Pigeon, any of several hundred species of birds constituting the family Columbidae (order Columbiformes). Smaller forms are usually called doves, larger forms pigeons. An exception is the white domestic pigeon, the symbol known as the “dove of peace.” \n"+
+                        "Pigeons occur worldwide except in the coldest regions and the most remote islands. About 250 species are known; two-thirds of them occur in tropical Southeast Asia, Australia, and the islands of the western Pacific, but the family also has many members in Africa and South America and a few in temperate Eurasia and North America. All members of the family suck liquids, rather than sip and swallow as do other birds, and all pigeon parents feed their young “pigeon’s milk,” the sloughed-off lining of the crop, the production of which is stimulated by the hormone prolactin. The nestling obtains this “milk” by poking its bill down the parent’s throat.",
+                "Eagle, any of many large, heavy-beaked, big-footed birds of prey belonging to the family Accipitridae (order Falconiformes). In general, an eagle is any bird of prey more powerful than a buteo. An eagle may resemble a vulture in build and flight characteristics but has a fully feathered (often crested) head and strong feet equipped with great curved talons. A further difference is in foraging habits: eagles subsist mainly on live prey. They are too ponderous for effective aerial pursuit but try to surprise and overwhelm their prey on the ground. Like owls, many decapitate their kills. Because of their strength, eagles have been a symbol of war and imperial power since Babylonian times. Their likeness is found on Greek and Roman ruins, coins, and medals.",
+                "Crane, any of 15 species of tall wading birds of the family Gruidae (order Gruiformes). Superficially, cranes resemble herons but usually are larger and have a partly naked head, a heavier bill, more compact plumage, and an elevated hind toe. In flight the long neck is stretched out in front, the stiltlike legs trailing out behind. \n"+
+                        "Cranes form an ancient group, the earliest fossils having been recovered from Eocene deposits in North America. Living forms are found worldwide except in South America, but populations of many are endangered by hunting and habitat destruction.\n" +
+                        "\n" +
+                        "These graceful terrestrial birds stalk about in marshes and on plains, eating small animals of all sorts as well as grain and grass shoots. Two olive-gray eggs spotted with brown are laid in a nest of grasses and weed stalks on drier ground in marsh or field. The same nest may be used year after year. The brownish, downy young can run about shortly after hatching. The trachea (windpipe) is simple in the chick but lengthens with age, coiling upon itself like a French horn. It lies buried in the hollow keel of the breastbone and reaches a length of 1.5 metres (5 feet) in the adult whooping crane (Grus americana)............................................................."
+        };
+
         ListAdapter listAdapter = new ListAdapter(MainActivity.this,arrBirdNames,arrBirdPics);
         lstBirds.setAdapter(listAdapter);
 
@@ -42,6 +63,7 @@ public class MainActivity extends AppCompatActivity {
                 Intent intent = new Intent(MainActivity.this,DetailActivity.class);
                 intent.putExtra("birdName",arrBirdNames[position]);
                 intent.putExtra("birdPic",arrBirdPics[position]);
+                intent.putExtra("birdDetails",arrBirdDetails[position]);
                 startActivity(intent);
             }
         });
