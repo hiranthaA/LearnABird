5d285e94deee5e508ad3819c734e3c4f74c4028f
hiran <hiranthaathapaththu@gmail.com>
2019-05-05 06:13:37 +0530

    sound play, stop, record, rec stop functions completed

--- app/src/main/java/com/example/learnabird/AddBird.java
+++ app/src/main/java/com/example/learnabird/AddBird.java
@@ -1,28 +1,36 @@
 package com.example.learnabird;
 
 import android.Manifest;
+import android.annotation.TargetApi;
 import android.content.ContentValues;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.media.MediaPlayer;
+import android.media.MediaRecorder;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.Toast;
+
+import java.io.IOException;
+import java.util.UUID;
 
 public class AddBird extends AppCompatActivity {
 
     private static final int PERMISSION_CODE_BROWSE = 1000;
     private static final int PERMISSION_CODE_CAMERA = 1001;
+    private static final int PERMISSION_CODE_SOUND_RECODING = 1004;
     private static final int IMAGE_PICK_CODE = 1002;
     private static final int IMAGE_CAPTURE_CODE = 1003;
+
     ImageButton btnCamera;
     ImageButton btnImageBrowse;
     ImageView imgPreview;
@@ -30,7 +38,13 @@ public class AddBird extends AppCompatActivity {
     ImageButton btnRecStop;
     ImageButton btnSoundBrowse;
     Uri img_uri;
+    String recPathSave = "";
+    MediaRecorder mediaRecorder;
+    MediaPlayer mediaPlayer;
+    boolean recStatus = false;
+    boolean playStatus = false;
 
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -84,23 +98,103 @@ public class AddBird extends AppCompatActivity {
         btnPlayStop.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                if(playStatus){
+                    if(mediaPlayer != null){
+                        mediaPlayer.stop();
+                        mediaPlayer.release();
+                    }
+                    playStatus = false;
+                }
+                else{
+                    mediaPlayer = new MediaPlayer();
+                    try {
+                        mediaPlayer.setDataSource(recPathSave);
+                        mediaPlayer.prepare();
+                        mediaPlayer.start();
+                        Toast.makeText(AddBird.this,"Playing sound preview...",Toast.LENGTH_SHORT).show();
+                        playStatus = true;
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                        Toast.makeText(AddBird.this,"No sound selected...",Toast.LENGTH_SHORT).show();
+                        playStatus = false;
+                    }
+                }
             }
         });
 
         btnRecStop.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                if (Build.VERSION.SDK_INT >= 23){
+                    if(checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE)== PackageManager.PERMISSION_DENIED ||
+                            checkSelfPermission(Manifest.permission.RECORD_AUDIO)==PackageManager.PERMISSION_DENIED){
+                        String[] permissions = {Manifest.permission.WRITE_EXTERNAL_STORAGE,Manifest.permission.RECORD_AUDIO};
+                        requestPermissions(permissions,PERMISSION_CODE_SOUND_RECODING);
+                    }
+                    else{
+                        recordSound();
+                    }
+                }
+                else{
+                    recordSound();
+                }
             }
         });
 
         btnSoundBrowse.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                
+
             }
         });
+
+    }
+
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+    private void recordSound(){
+        if(recStatus){
+            //while recording press button
+            mediaRecorder.stop();
+            Toast.makeText(AddBird.this,"Recording Stopped.",Toast.LENGTH_SHORT).show();
+            btnRecStop.setBackgroundTintList(this.getResources().getColorStateList(R.color.colorPrimary));
+            btnPlayStop.setBackgroundTintList(this.getResources().getColorStateList(R.color.colorPrimary));
+            btnPlayStop.setEnabled(true);
+            btnSoundBrowse.setBackgroundTintList(this.getResources().getColorStateList(R.color.colorPrimary));
+            btnSoundBrowse.setEnabled(true);
+            btnImageBrowse.setBackgroundTintList(this.getResources().getColorStateList(R.color.colorPrimary));
+            btnImageBrowse.setEnabled(true);
+            btnCamera.setBackgroundTintList(this.getResources().getColorStateList(R.color.colorPrimary));
+            btnCamera.setEnabled(true);
+
+            recStatus = false;
+        }
+        else{
+            //while not recording press button
+            recPathSave = Environment.getExternalStorageDirectory().getAbsolutePath()+"/"+ UUID.randomUUID().toString()+"_audio_record.3gp";
+            mediaRecorder = new MediaRecorder();
+            mediaRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+            mediaRecorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
+            mediaRecorder.setAudioEncoder(MediaRecorder.OutputFormat.AMR_NB);
+            mediaRecorder.setOutputFile(recPathSave);
+            try {
+                mediaRecorder.prepare();
+                mediaRecorder.start();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            Toast.makeText(AddBird.this,"Recording...",Toast.LENGTH_SHORT).show();
+            btnRecStop.setBackgroundTintList(this.getResources().getColorStateList(R.color.colorRed));
+            btnPlayStop.setBackgroundTintList(this.getResources().getColorStateList(R.color.imgBackground));
+            btnPlayStop.setEnabled(false);
+            btnSoundBrowse.setBackgroundTintList(this.getResources().getColorStateList(R.color.imgBackground));
+            btnSoundBrowse.setEnabled(false);
+            btnImageBrowse.setBackgroundTintList(this.getResources().getColorStateList(R.color.imgBackground));
+            btnImageBrowse.setEnabled(false);
+            btnCamera.setBackgroundTintList(this.getResources().getColorStateList(R.color.imgBackground));
+            btnCamera.setEnabled(false);
+
+            recStatus = true;
+        }
 
     }
 
@@ -132,6 +226,7 @@ public class AddBird extends AppCompatActivity {
                     Toast.makeText(AddBird.this,"Permission denied...",Toast.LENGTH_SHORT).show();
                 }
             }
+            break;
             case PERMISSION_CODE_CAMERA : {
                 if (grantResults.length > 0 && grantResults[0]==PackageManager.PERMISSION_GRANTED){
                     openCamera();
@@ -140,6 +235,16 @@ public class AddBird extends AppCompatActivity {
                     Toast.makeText(AddBird.this,"Permission denied...",Toast.LENGTH_SHORT).show();
                 }
             }
+            break;
+            case PERMISSION_CODE_SOUND_RECODING :{
+                if (grantResults.length > 0 && grantResults[0]==PackageManager.PERMISSION_GRANTED){
+                    recordSound();
+                }
+                else{
+                    Toast.makeText(AddBird.this,"Permission denied...",Toast.LENGTH_SHORT).show();
+                }
+            }
+            default:Toast.makeText(AddBird.this,"All Permissions denied...",Toast.LENGTH_SHORT).show();
         }
     }
 
