bbc892a99086b1be176bef1ca2a70c6e492c2642
hiran <hiranthaathapaththu@gmail.com>
2019-05-08 17:11:59 +0530

    db data now loads in list as well as detail view

--- app/src/main/java/com/example/learnabird/ListAdapter.java
+++ app/src/main/java/com/example/learnabird/ListAdapter.java
@@ -1,6 +1,9 @@
 package com.example.learnabird;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Environment;
 import android.support.annotation.NonNull;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -11,16 +14,20 @@ import android.widget.TextView;
 
 import com.example.learnabird.AsyncTasks.AsyncLoadImage;
 
+import java.io.IOException;
+
 public class ListAdapter extends ArrayAdapter {
 
     private String[] birdNames;
     private String[] birdPics;
+    private String[] locations;
     private Context context;
-
-    public ListAdapter(Context context, String[] names, String[] pics) {
+    private String storage="/storage/emulated/0/Android/data/com.example.learnabird/files/Pictures/";
+    public ListAdapter(Context context, String[] names, String[] pics, String[] locations) {
         super(context, R.layout.list_view_item);
         this.birdNames = names;
         this.birdPics = pics;
+        this.locations = locations;
         this.context = context;
     }
 
@@ -43,7 +50,14 @@ public class ListAdapter extends ArrayAdapter {
         else {
             viewHolder = (ViewHolder)convertView.getTag();
         }
-        new AsyncLoadImage(viewHolder.mBird).execute(birdPics[position]);
+        if(locations[position].equals("api")) {
+            new AsyncLoadImage(viewHolder.mBird).execute(birdPics[position]);
+        }
+        else{
+            //load data from db
+            Bitmap bitmap = BitmapFactory.decodeFile(storage+birdPics[position]);
+            viewHolder.mBird.setImageBitmap(bitmap);
+        }
         viewHolder.mName.setText(birdNames[position]);
         return convertView;
     }
--- app/src/main/java/com/example/learnabird/MainActivity.java
+++ app/src/main/java/com/example/learnabird/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.learnabird;
 
 import android.content.Intent;
+import android.database.Cursor;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -8,6 +9,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 import org.json.JSONArray;
@@ -20,6 +22,7 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -29,6 +32,14 @@ public class MainActivity extends AppCompatActivity {
     private String[] arrBirdPics;
     private String[] arrBirdDetails;
     private String[] arrBirdSounds;
+
+    private String[] arrLocation;
+
+    private ArrayList<String> arrlstDbBirdNames;
+    private ArrayList<String> arrlstDbBirdInfo;
+    private ArrayList<String> arrlstDbBirdPhotos;
+    private ArrayList<String> arrlstDbBirdSounds;
+
     public static String host;
     private int[] arrBirdIds;
     private DatabaseHelper db;
@@ -44,6 +55,8 @@ public class MainActivity extends AppCompatActivity {
         host = "https://learn-a-bird-server.herokuapp.com/";
         String getDataURL = host+"bird/getall";
 
+        //Load data from database
+        loadDbData();
         //Load data from api asynchronously
         new LoadApiData().execute(getDataURL);
 
@@ -63,6 +76,7 @@ public class MainActivity extends AppCompatActivity {
                 intent.putExtra("birdDetails",arrBirdDetails[position]);
                 intent.putExtra("birdSound", arrBirdSounds[position]);
                 intent.putExtra("birdId",arrBirdIds[position]);
+                intent.putExtra("location",arrLocation[position]);
                 startActivity(intent);
             }
         });
@@ -75,6 +89,28 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+    }
+
+    public void loadDbData(){
+        arrlstDbBirdNames = new ArrayList<String>();
+        arrlstDbBirdInfo = new ArrayList<String>();
+        arrlstDbBirdPhotos = new ArrayList<String>();
+        arrlstDbBirdSounds = new ArrayList<String>();
+        Cursor data = db.getBirdList();
+
+        if(data!=null){
+            if(data.getCount()>0){
+                while (data.moveToNext()){
+                    arrlstDbBirdNames.add(data.getString(1));
+                    arrlstDbBirdInfo.add(data.getString(2));
+                    arrlstDbBirdPhotos.add(data.getString(3));
+                    arrlstDbBirdSounds.add(data.getString(4));
+//                    android.widget.ListAdapter listAdapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,alarmlist);
+//                    alarmListView.setAdapter(listAdapter);
+                }
+            }
+        }
+        System.out.println("data loaded");
     }
 
     public class LoadApiData extends AsyncTask<String, Void, String> {
@@ -119,25 +155,44 @@ public class MainActivity extends AppCompatActivity {
 
             try {
                 JSONArray jsonArray = new JSONArray(result);
-                arrBirdNames = new String[jsonArray.length()];
-                arrBirdDetails = new String[jsonArray.length()];
-                arrBirdPics = new String[jsonArray.length()];
-                arrBirdSounds = new String[jsonArray.length()];
-                arrBirdIds = new int[jsonArray.length()];
-                for(int i=0 ; i<jsonArray.length(); i++){
+                int apiDataSize = jsonArray.length();
+                int dbDataSize = arrlstDbBirdNames.size();
+
+                arrBirdNames = new String[jsonArray.length()+dbDataSize];
+                arrBirdDetails = new String[jsonArray.length()+dbDataSize];
+                arrBirdPics = new String[jsonArray.length()+dbDataSize];
+                arrBirdSounds = new String[jsonArray.length()+dbDataSize];
+                arrBirdIds = new int[jsonArray.length()+dbDataSize];
+                arrLocation = new String[jsonArray.length()+dbDataSize];
+
+                for(int i=0 ; i<apiDataSize; i++){
                     JSONObject jsonBird = jsonArray.getJSONObject(i);
-                    jsonBird.getInt("id");
+                    arrBirdIds[i] = jsonBird.getInt("id");
                     arrBirdNames[i]= jsonBird.getString("name");
                     arrBirdDetails[i]= jsonBird.getString("info");
                     arrBirdPics[i]=jsonBird.getString("photo");
                     arrBirdSounds[i]=jsonBird.getString("sound");
+                    arrLocation[i]="api";
                 }
+
+                //merge db data to api data array
+                int count = apiDataSize;
+                for(int i=0; i< dbDataSize; i++){
+                    arrBirdNames[count] = arrlstDbBirdNames.get(i);
+                    arrBirdDetails[count] = arrlstDbBirdInfo.get(i);
+                    arrBirdPics[count] = arrlstDbBirdPhotos.get(i);
+                    arrBirdSounds[count] = arrlstDbBirdSounds.get(i);
+                    arrLocation[count] = "db";
+                    count++;
+                }
+
+                System.out.println("merging lists ok");
 
             } catch (JSONException e) {
                 e.printStackTrace();
             }
 
-            ListAdapter listAdapter = new ListAdapter(MainActivity.this,arrBirdNames, arrBirdPics);
+            ListAdapter listAdapter = new ListAdapter(MainActivity.this,arrBirdNames, arrBirdPics, arrLocation);
             lstBirds.setAdapter(listAdapter);
 
         }
